580c46a20b07a43291ed5489de87c63246f3b997
sumitghugare1 <149884262+sumitghugare1@users.noreply.github.com>
2024-02-16 18:40:56 +0530

    Add files via upload

---
+++ hello.js
@@ -0,0 +1,11 @@
+const name= "cli"
+console.log(name);
+//console.log(global);
+
+const add =10+2
+console.log(" the added value is=",add)
+//the output as : the total sum is ()
+const subs = 10-2
+console.log("the substracted value=",subs)
+//output as : the substracted value ()
+
